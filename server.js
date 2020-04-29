9a5fb4b78a64150b5d745e488a9426c18dae21ed
DanNguyen8189 <dannguyen8189@gmail.com>
2020-04-29 15:52:11 -0700

    Full functionality added, trying a deploy

--- server.js
+++ server.js
@@ -28,12 +28,11 @@ let REDIRECT_URI = process.env.REDIRECT_URI || "http://localhost:5000/callback";
 let FRONTEND_URI = process.env.FRONTEND_URI || "http://localhost:8080";
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "development") {
     REDIRECT_URI = "http://localhost:5000/callback";
     FRONTEND_URI = "http://localhost:8080";
 }
-FRONTEND_URI="https://hotspotify.herokuapp.com/";
-REDIRECT_URI="https://hotspotify.herokuapp.com/callback";
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
